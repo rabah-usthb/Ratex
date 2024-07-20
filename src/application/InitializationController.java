93c6dbbf376c361aeffd6c2a6a4e6d243fd8da1e
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-07-20 18:02:32 +0200

    styled all the textfield + added input listener to only let float input in field + added event happening when shift+arrowUp increment value , shift+arrowDown to decrement value

--- src/application/InitializationController.java
+++ src/application/InitializationController.java
@@ -1,10 +1,10 @@
 package application;
 
 import java.net.URL;
-
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.ResourceBundle;
-
-
+import java.util.function.UnaryOperator;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,9 +12,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+
 
 public class InitializationController implements Initializable {
-
+	 static boolean Updated = false;
 	private static String DocumentType[] = {"article","report","proc","book","letter","memoir","beamer","minimal","slides"}; 
     @FXML
     private ComboBox<String> TypeComboBox;
@@ -47,6 +52,46 @@ TextField BottomField;
 
 static int clicked =0;
 
+
+private void setupKeyEventHandling(TextField field) {
+    field.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+        if (isNowFocused) {
+            field.addEventHandler(KeyEvent.KEY_PRESSED, event -> handleKeyEvent(event, field));
+        } else {
+            field.removeEventHandler(KeyEvent.KEY_PRESSED, event -> handleKeyEvent(event, field));
+        }
+    });
+}
+
+private void handleKeyEvent(KeyEvent event,TextField field) {
+    if (event.isShiftDown() && event.getCode() == KeyCode.UP) {
+        incrementField(field);
+    }
+    else if(event.isShiftDown() && event.getCode() == KeyCode.DOWN) {
+        decrementField(field);
+    }
+}
+
+private void decrementField(TextField field) {
+	double number =Double.parseDouble(field.getText());
+	number = number-0.1;
+	field.setText(String.valueOf(number));
+}
+
+private void incrementField(TextField field) {
+	double number =Double.parseDouble(field.getText());
+	number = number+0.1;
+	field.setText(String.valueOf(number));
+}
+
+private boolean IsFloat(String text) {
+	String regex="\\s*(\\+\\s*)?\\d+(\\s*\\.\\s*\\d+)?\\s*";
+	String reg="\\s*\\+\\s*";
+	String rag="\\s*(\\+\\s*)?\\d+(\\s*\\.\\s*)?";
+	return text.matches(regex)||text.matches(reg)||text.matches(rag);
+}
+
+
 private void setVisibilityMono(boolean Isvisible) {
 	MarginLabel.setVisible(Isvisible);
 	MarginField.setVisible(Isvisible);
@@ -76,8 +121,33 @@ private void switchButtonToPoly() {
 	}
 	
 }
+
+private void setUpInputListener(TextField field) {
+
+	field.textProperty().addListener((obs, oldText, newText) -> {	
+		boolean updated = false;
+		if(!updated&&!newText.isBlank()) {
+       String AllText = field.getText(); 	
+	if (!IsFloat(AllText)) {
+    	
+    	field.setText(oldText);
+    	updated = true;	
+    }
+	else {
+		
+    	updated = true;
+	}
+	
+	
+	    
+	}
+
+});
+
+} 
+
 @FXML
- private  void switchButtonToMono() {
+ private void switchButtonToMono() {
 	if(clicked==1) {
 		setVisibilityMono(true);
 		setVisibilityPoly(false);
@@ -94,5 +164,18 @@ private void switchButtonToPoly() {
 		for(String type :DocumentType) {
 			TypeComboBox.getItems().add(type);
 		}
+		
+			
+		
+		setupKeyEventHandling(MarginField);
+		setUpInputListener(TopField);
+		setUpInputListener(MarginField);
+		setUpInputListener(LeftField);
+		setUpInputListener(RightField);
+		setUpInputListener(BottomField);
 	}
+
+	
+    
+
 }
